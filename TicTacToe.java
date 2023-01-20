30e25cf4cedff6624f98141f489431c665cefee8
david <davidjhoyjr@gmail.com>
2023-01-20 10:03:44 -0500

    panel layout and buttons

---
+++ TicTacToe.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener{
+    
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel text_field = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+    
+
+    TicTacToe(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(new Color(50, 50, 50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        text_field.setBackground(new Color(25, 25, 25));
+        text_field.setForeground(new Color(25, 255, 5));
+        // text_field.setFont(new Font("Ink Free", Font.BOLD, 75));
+        text_field.setHorizontalAlignment(JLabel.CENTER);
+        text_field.setText("Tic-Tac-Toe");
+        text_field.setOpaque(false);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0, 0, 800, 100);
+
+        button_panel.setLayout(new GridLayout(3,3));
+        button_panel.setBackground(new Color(150, 150, 150));
+
+        for (int i = 0; i < 9; i++){
+            buttons[i] = new JButton();
+            button_panel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli", Font.BOLD, 100));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+
+        }
+
+        title_panel.add(text_field);
+        frame.add(title_panel, BorderLayout.NORTH);
+        frame.add(button_panel);
+        
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(int a, int b, int c){
+
+    }
+
+
+
+}
